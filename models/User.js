abe368b9d85e6cc76bfc43aa42c42a2e2860717e
caldardn <caldardn@gmail.com>
2024-01-09 11:27:33 -0500

    adding User model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require('mongoose'); 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    // array of _id values referencing the Thought model
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    // array of _id values referencing the User model (self-reference)
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+model.exports = User;
